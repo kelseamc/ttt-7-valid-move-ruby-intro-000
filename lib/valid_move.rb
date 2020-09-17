defc7fd2bc2d0952f1b71621592dbdf2cd2a18c2
Kelsea McAllister <kelsea.mcall@gmail.com>
2020-09-17 16:44:59 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -12,6 +12,7 @@ def valid_position(index)
     return true
   else
     return false
+  end
 end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
