89d849d2814aea8a130c1c3d03dff6b32de04362
Kelsea McAllister <kelsea.mcall@gmail.com>
2020-09-17 16:33:09 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,17 @@
 # code your #valid_move? method here
-
+def valid_move
+  if position_taken? == false
+    return true
+  else
+    return false 
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
